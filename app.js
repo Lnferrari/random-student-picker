7acc5f8ba2940bfb9f049565d4f40ac07112a6ee
Lucas Ferrari <ln.ferrari@hotmail.com>
2021-07-02 13:27:13 +0200

    added media queries. Almost done with the random name animation, is still throwing an error

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 // variables
+let counter = 0
 const students = ['Aimee', 'Beda', 'Carlos', 'Charli', 'Christoph', 'Dalibor', 'Daniel', 'Francesco', 'Habid', 'Jonathan', 'Lucas', 'Lui', 'Martin', 'Mo', 'Norman', 'Priya', 'Shinhee', 'Shreekesh', 'Tim']
 const deleteIcon = '<i class="far fa-trash-alt delete"></i>'
 const display = document.querySelector('#display')
@@ -10,6 +11,69 @@ const editBtn = document.querySelector('#edit')
 const addBtn = document.querySelector('#add')
 const closeBtn = document.querySelector('#close')
 const pickOneBtn = document.querySelector('#pickOne')
+
+// Class
+class TextScramble {
+    constructor(el) {
+        this.el = el
+        this.chars = '$!<>-_\\/[]@&{}—=+*^?#________'
+        // this.chars = '$!<>-_\\/[]@&{}—=+*^?#________aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ'
+        this.update = this.update.bind(this)
+    }
+
+    setText(newText) {
+        const oldText = this.el.innerText
+        //const length = students.length
+        const length = Math.max(oldText.length, newText.length)
+        const promise = new Promise((resolve) => this.resolve = resolve)
+        this.queue = []
+        for (let i = 0; i < length; i++) {
+            const from = oldText[i] || '';
+            const to = newText[i] || '';
+            const start = Math.floor(Math.random() * 40)
+            const end = start + Math.floor(Math.random() * 40)
+            this.queue.push({ from, to, start, end })
+        }
+
+        cancelAnimationFrame(this.frameRequest)
+        this.frame = 0
+        this.update()
+        return promise
+    }
+
+    update() {
+        let output = ''
+        let complete = 0
+        for (let i = 0, n = this.queue.length; i < n; i++) {
+            let { from, to, start, end, char } = this.queue[i]
+            if (this.frame >= end) {
+                complete++
+                output += to
+            } else if (this.frame >= start) {
+                if (!char || Math.random() < 0.28) {
+                    char = this.randomChar()
+                    this.queue[i].char = char
+                }
+                output += `<span class="symbol">${char}</span>`
+            } else {
+                output += from
+            }
+        }
+        this.el.innerHTML = output
+        if (complete === this.queue.length) {
+            this.resolve()
+        } else {
+            this.frameRequest = requestAnimationFrame(this.update)
+            this.frame++
+        }
+    }
+    
+    randomChar() {
+        return this.chars[Math.floor(Math.random() * this.chars.length)]
+    }
+}
+// instance
+const scrambleText = new TextScramble(display)
 
 // functions
 const getRandom = array => array[Math.floor(Math.random() * array.length)];
@@ -26,6 +90,18 @@ const removeItem = item => students.splice(students.indexOf(item), 1);
 const pickOne = () => {
     return getRandom(students)    
 }
+
+const next = () => {
+    scrambleText.setText(students[counter]).then(() => {
+        setTimeout(next)
+    })
+    counter = Math.floor(Math.random() * students.length)
+}
+
+const stop = (item) => {
+  scrambleText.setText(item)
+}
+
 
 // events
 window.addEventListener('load', ()=> students.forEach(item => displayOnList(item)))
@@ -48,7 +124,6 @@ listContainer.addEventListener('click', (e) => {
     } else if (e.target.className.includes('delete')){
         e.target.parentElement.remove()
         removeItem(e.target.previousSibling.textContent)
-        console.log(students)
     }
 })
 
@@ -58,12 +133,16 @@ resetBtn.addEventListener('click', ()=>{
 
 pickOneBtn.addEventListener('click', ()=>{
     const student = pickOne()
+    next()
+    setTimeout(()=> stop(student), 6500)
     removeItem(student)
     Array.from(list.children).forEach(item => {
         if (item.innerText === student){
-            item.firstElementChild.style.textDecoration = 'line-through'
-            item.firstElementChild.style.textDecorationColor = 'red'
-            item.firstElementChild.style.textDecorationThickness = '3px'
+            item.firstElementChild.style.color = '#757575';
         }
     })
 })
+
+
+  
+
